01fc3854ef9384bb15426dec19fe1f12d723b630
Bradley Duncan <bradleydunc@gmail.com>
2014-03-03 14:31:00 +0000

    Changes to the variable names (TO BE TESTED)
    Signed-off-by: Bradley Duncan <bradleydunc@gmail.com>

--- src/java_assignment/TEXTANALYSE.java
+++ src/java_assignment/TEXTANALYSE.java
@@ -15,7 +15,7 @@ public strictfp class TEXTANALYSE extends Applet implements ActionListener {
     Button resetButton;      //Reset button
     String inputText;
     int stringLength1, stringLength2, stringLength3, stringLength4, stringLength5, stringLength6, stringLength7, stringLength8, stringLength9;    //Variables with the string lengths stored for future comparison
-    double meanLength, stringCounter, SLB1, SLB2, SLB3, SLB4, SLB5, SLB6, SLB7, SLB8, SLB9, SLB11, SLB22, SLB33, SLB44, SLB55, SLB66, SLB77, SLB88, SLB99;//used for working out coordinates
+    double meanLength, stringCounter, lengthBar1, lengthBar2, lengthBar3, lengthBar4, lengthBar5, lengthBar6, lengthBar7, lengthBar8, lengthBar9, lengthBar11, lengthBar22, lengthBar33, lengthBar44, lengthBar55, lengthBar66, lengthBar77, lengthBar88, lengthBar99;//used for working out coordinates
 
 
     public void init() {
@@ -42,50 +42,50 @@ public strictfp class TEXTANALYSE extends Applet implements ActionListener {
         g.drawLine(50, 300, 410, 300);     //Drawing the x-axis
         g.drawLine(50, 300, 50, 50);       //Drawing the y-axis
         g.setColor(Color.red);                            //Setting the bar color
-        g.drawRect(50, (int) SLB11, 40, (int) SLB1); //Drawing the bar
+        g.drawRect(50, (int) lengthBar11, 40, (int) lengthBar1); //Drawing the bar
         g.setColor(Color.black);                   //Setting the text color
         g.drawString("1", 70, 315);                 //Giving values to the x-axis
-        g.drawString("" + stringLength1 + "", 40, (int) SLB11);      //Displaying the frequency of the word on the y-axis
+        g.drawString("" + stringLength1 + "", 40, (int) lengthBar11);      //Displaying the frequency of the word on the y-axis
         g.setColor(Color.orange);
-        g.drawRect(90, (int) SLB22, 40, (int) SLB2); //stringLength2
+        g.drawRect(90, (int) lengthBar22, 40, (int) lengthBar2); //stringLength2
         g.setColor(Color.black);
         g.drawString("2", 110, 315);
-        g.drawString("" + stringLength2 + "", 40, (int) SLB22);
+        g.drawString("" + stringLength2 + "", 40, (int) lengthBar22);
         g.setColor(Color.yellow);
-        g.drawRect(130, (int) SLB33, 40, (int) SLB3); //stringLength3
+        g.drawRect(130, (int) lengthBar33, 40, (int) lengthBar3); //stringLength3
         g.setColor(Color.black);
         g.drawString("3", 150, 315);
-        g.drawString("" + stringLength3 + "", 40, (int) SLB33);
+        g.drawString("" + stringLength3 + "", 40, (int) lengthBar33);
         g.setColor(Color.green);
-        g.drawRect(170, (int) SLB44, 40, (int) SLB4); //stringLength4
+        g.drawRect(170, (int) lengthBar44, 40, (int) lengthBar4); //stringLength4
         g.setColor(Color.black);
         g.drawString("4", 190, 315);
-        g.drawString("" + stringLength4 + "", 40, (int) SLB44);
+        g.drawString("" + stringLength4 + "", 40, (int) lengthBar44);
         g.setColor(Color.blue);
-        g.drawRect(210, (int) SLB55, 40, (int) SLB5); //stringLength5
+        g.drawRect(210, (int) lengthBar55, 40, (int) lengthBar5); //stringLength5
         g.setColor(Color.black);
         g.drawString("5", 230, 315);
-        g.drawString("" + stringLength5 + "", 40, (int) SLB55);
+        g.drawString("" + stringLength5 + "", 40, (int) lengthBar55);
         g.setColor(Color.red);
-        g.drawRect(250, (int) SLB66, 40, (int) SLB6); //stringLength6
+        g.drawRect(250, (int) lengthBar66, 40, (int) lengthBar6); //stringLength6
         g.setColor(Color.black);
         g.drawString("6", 270, 315);
-        g.drawString("" + stringLength6 + "", 40, (int) SLB66);
+        g.drawString("" + stringLength6 + "", 40, (int) lengthBar66);
         g.setColor(Color.orange);
-        g.drawRect(290, (int) SLB77, 40, (int) SLB7); //stringLength7
+        g.drawRect(290, (int) lengthBar77, 40, (int) lengthBar7); //stringLength7
         g.setColor(Color.black);
         g.drawString("7", 310, 315);
-        g.drawString("" + stringLength7 + "", 40, (int) SLB77);
+        g.drawString("" + stringLength7 + "", 40, (int) lengthBar77);
         g.setColor(Color.yellow);
-        g.drawRect(330, (int) SLB88, 40, (int) SLB8); //stringLength8
+        g.drawRect(330, (int) lengthBar88, 40, (int) lengthBar8); //stringLength8
         g.setColor(Color.black);
         g.drawString("8", 350, 315);
-        g.drawString("" + stringLength8 + "", 40, (int) SLB88);
+        g.drawString("" + stringLength8 + "", 40, (int) lengthBar88);
         g.setColor(Color.green);
-        g.drawRect(370, (int) SLB99, 40, (int) SLB9); //stringLength9
+        g.drawRect(370, (int) lengthBar99, 40, (int) lengthBar9); //stringLength9
         g.setColor(Color.black);
         g.drawString("9", 390, 315);
-        g.drawString("" + stringLength9 + "", 40, (int) SLB99);
+        g.drawString("" + stringLength9 + "", 40, (int) lengthBar99);
         g.drawString("Mean word length is: " + meanLength, 200, 330);
     }
 
@@ -114,42 +114,42 @@ public strictfp class TEXTANALYSE extends Applet implements ActionListener {
                 stringLength9 += 1;
             meanLength = ((stringLength9 * 9) + (stringLength8 * 8) + (stringLength7 * 7) + (stringLength6 * 6) + (stringLength5 * 5) + (stringLength4 * 4) + (stringLength3 * 3) + (stringLength2 * 2) + (stringLength1 * 1)) / stringCounter;
             //^^^Working out the Mean Word Length
-            SLB1 = ((250 / stringCounter) * stringLength1);  //Working out the coordinates of each bar
-            SLB11 = (300 - SLB1);
-            SLB1 = (300 - SLB11);
+            lengthBar1 = ((250 / stringCounter) * stringLength1);  //Working out the coordinates of each bar
+            lengthBar11 = (300 - lengthBar1);
+            lengthBar1 = (300 - lengthBar11);
 
 
-            SLB2 = ((250 / stringCounter) * stringLength2);
-            SLB22 = (300 - SLB2);
-            SLB2 = (300 - SLB22);
+            lengthBar2 = ((250 / stringCounter) * stringLength2);
+            lengthBar22 = (300 - lengthBar2);
+            lengthBar2 = (300 - lengthBar22);
 
-            SLB3 = ((250 / stringCounter) * stringLength3);
-            SLB33 = (300 - SLB3);
-            SLB3 = (300 - SLB33);
+            lengthBar3 = ((250 / stringCounter) * stringLength3);
+            lengthBar33 = (300 - lengthBar3);
+            lengthBar3 = (300 - lengthBar33);
 
-            SLB4 = ((250 / stringCounter) * stringLength4);
-            SLB44 = (300 - SLB4);
-            SLB4 = (300 - SLB44);
+            lengthBar4 = ((250 / stringCounter) * stringLength4);
+            lengthBar44 = (300 - lengthBar4);
+            lengthBar4 = (300 - lengthBar44);
 
-            SLB5 = ((250 / stringCounter) * stringLength5);
-            SLB55 = (300 - SLB5);
-            SLB5 = (300 - SLB55);
+            lengthBar5 = ((250 / stringCounter) * stringLength5);
+            lengthBar55 = (300 - lengthBar5);
+            lengthBar5 = (300 - lengthBar55);
 
-            SLB6 = ((250 / stringCounter) * stringLength6);
-            SLB66 = (300 - SLB6);
-            SLB6 = (300 - SLB66);
+            lengthBar6 = ((250 / stringCounter) * stringLength6);
+            lengthBar66 = (300 - lengthBar6);
+            lengthBar6 = (300 - lengthBar66);
 
-            SLB7 = ((250 / stringCounter) * stringLength7);
-            SLB77 = (300 - SLB7);
-            SLB7 = (300 - SLB77);
+            lengthBar7 = ((250 / stringCounter) * stringLength7);
+            lengthBar77 = (300 - lengthBar7);
+            lengthBar7 = (300 - lengthBar77);
 
-            SLB8 = ((250 / stringCounter) * stringLength8);
-            SLB88 = (300 - SLB8);
-            SLB8 = (300 - SLB88);
+            lengthBar8 = ((250 / stringCounter) * stringLength8);
+            lengthBar88 = (300 - lengthBar8);
+            lengthBar8 = (300 - lengthBar88);
 
-            SLB9 = ((250 / stringCounter) * stringLength9);
-            SLB99 = (300 - SLB9);
-            SLB9 = (300 - SLB99);
+            lengthBar9 = ((250 / stringCounter) * stringLength9);
+            lengthBar99 = (300 - lengthBar9);
+            lengthBar9 = (300 - lengthBar99);
 
         }
         if (e.getSource() == resetButton) {
